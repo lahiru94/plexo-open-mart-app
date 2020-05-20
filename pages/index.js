2c6822773ec0643b119243e545006802d08dfe48
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2020-05-20 11:26:05 +0530

    home page improvements

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,8 @@ export default function Home() {
 
     return (
         <div className="container">
-            <section style={{ marginTop: 60 }}>
+            <section style={{ marginTop: 60, display:'flex', justifyContent:'center' }}>
+                <div className="col-12 col-sm-12 col-md-10">
                 <div className="form-group">
                     <div className="input-group mb-4">
                         <input className="form-control" placeholder="Search for model" type="text" />
@@ -17,14 +18,15 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+                </div>
             </section>
 
             <section style={{ marginTop: 50 }}>
-                <h4>
-                    Featured
-                    </h4>
+                <h5 style={{ width:'100%', display:'flex', justifyContent:'center' }} >
+                    Featured Models
+                </h5>
                 <div className="d-flex flex-wrap justify-content-center" style={{ marginTop: 30 }}>
-                    {featuredModels.map((model, index) => (<ModelCard index={index + 1} />))}
+                    {featuredModels.map((model, index) => (<ModelCard key={index} index={index + 1} />))}
                 </div>
             </section>
         </div>
