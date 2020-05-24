13572ffcda916c695aa45f13feca63140fe1ca21
lahiru94 <lahirud.14@cse.mrt.ac.lk>
2020-05-24 18:53:35 +0530

    fix(config):base url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    // baseUrl:"https://plexo-open-mart-app.now.sh/api"
-    baseUrl:"http://localhost:3000/api"
+    baseUrl:"https://plexo-open-mart-app.now.sh/api"
+    // baseUrl:"http://localhost:3000/api"
 }
 
 export default config;
